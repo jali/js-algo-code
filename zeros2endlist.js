d9054aaf9beb17a8e254cf271a255523a33142e1
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2024-05-10 11:13:57 +0100

    add solution for find and move zeros to end of list problem

---
+++ zeros2endlist.js
@@ -0,0 +1,14 @@
+// From a list of numbers, move zero to the end of the list.
+
+const examine = (orginaList=[], resultList=[], zeroList=[]) => {
+    if (orginaList.length === 0) return [...resultList, ...zeroList];
+    
+    let niedle = orginaList.pop();
+    if (niedle === 0) {
+        return examine(orginaList, resultList, [...zeroList, niedle])
+    } else {
+        return examine(orginaList, [niedle, ...resultList], zeroList)
+    }
+}
+let result = examine([1,0,2,0,4,6]);
+console.log(result);
