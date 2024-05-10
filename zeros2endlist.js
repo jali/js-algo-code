66f4f535e63594a1322c754083c6bb24b306b8bb
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2024-05-10 12:50:17 +0100

    rename var

--- zeros2endlist.js
+++ zeros2endlist.js
@@ -1,14 +1,15 @@
 // From a list of numbers, move zero to the end of the list.
+// And the solution must be O(N)
 
-// solution recursive
+// Using recursive function
 const examine = (remainder=[], numbers=[], zeros=[]) => {
     if (remainder.length === 0) return [...numbers, ...zeros];
     
-    let niedle = remainder.pop();
-    if (niedle === 0) {
-        return examine(remainder, numbers, [...zeros, niedle])
+    let item = remainder.pop();
+    if (item === 0) {
+        return examine(remainder, numbers, [...zeros, item])
     } else {
-        return examine(remainder, [niedle, ...numbers], zeros)
+        return examine(remainder, [item, ...numbers], zeros)
     }
 }
 let result = examine([1,0,2,0,4,6]);
