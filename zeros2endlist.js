f829a0dab3855f6d49ac0c871601decad1aa29a8
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2024-05-13 10:16:23 +0100

    add bigO comments

--- zeros2endlist.js
+++ zeros2endlist.js
@@ -1,16 +1,23 @@
-// From a list of numbers, move zero to the end of the list.
-// And the solution must be O(N)
-
-// Using recursive function
-const examine = (remainder=[], numbers=[], zeros=[]) => {
-    if (remainder.length === 0) return [...numbers, ...zeros];
+// from a list of numbers, move zero to the end of the list.
+// solution must be O(N).
+//
+//   time complexity calculation
+//   traversing => O(N)
+//   pop => O(1) 
+//   spread operator => O(N)
+//   result => O(N)
+//
+// recursive function
+const examine = (tail=[], numbers=[], zeros=[]) => {
+    if (tail.length === 0) return [...numbers, ...zeros];
     
-    let item = remainder.pop();
-    if (item === 0) {
-        return examine(remainder, numbers, [...zeros, item])
+    let head = tail.pop();
+    if (head === 0) {
+        return examine(tail, numbers, [...zeros, head])
     } else {
-        return examine(remainder, [item, ...numbers], zeros)
+        return examine(tail, [head, ...numbers], zeros)
     }
-}
+};
+
 let result = examine([1,0,2,0,4,6]);
 console.log(result);
